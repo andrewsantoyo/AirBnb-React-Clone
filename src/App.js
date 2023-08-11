bca93db823f769de71df4ba016c90f5f182914cf
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-08-10 20:46:12 -0700

    Pass props to card component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Card from './components/Card';
+// Import images for Card prop.
+import katieZaferesImage from './images/katie-zaferes.png';
 
 function App() {
   return (
     <div>
       <Navbar /> 
       <Hero />
-      <Card />
+      <Card 
+        img={katieZaferesImage}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
-import katieZaferes from "../images/katie-zaferes.png"
 import star from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return ( 
         <div className="card">
-            <img src={katieZaferes} alt="Katie Zaferes" class="card--image"/>
+            <img src={props.img} alt="activity" class="card--image"/>
             <div class="card--stats">
                 <img src={star} alt="star" class="card--star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
