5dc76321cb6e254f99b80e251301b60f7ef6af15
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-08-10 22:31:08 -0700

    Add sold out and online badge

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,16 @@ import React from "react";
 import star from "../images/star.png"
 
 export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return ( 
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={props.img} alt="activity" class="card--image"/>
             <div class="card--stats">
                 <img src={star} alt="star" class="card--star"/>
