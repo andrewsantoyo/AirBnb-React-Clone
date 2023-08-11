88c388b64048fb85e06cd01b3990a1abff7fe6d7
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-08-10 21:50:59 -0700

    Map experiences data into components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Card from './components/Card';
-// Import images for Card prop.
-import katieZaferesImage from './images/katie-zaferes.png';
+import data from './data'
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        <Card 
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+}) 
+
   return (
     <div>
       <Navbar /> 
       <Hero />
-      <Card 
-        img={katieZaferesImage}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
