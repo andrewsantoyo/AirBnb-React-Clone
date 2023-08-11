3a1c2793cf20e574636680c84b2252299f00e343
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-08-10 22:38:29 -0700

    Refactor card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,24 @@ import star from "../images/star.png"
 
 export default function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
     return ( 
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={props.img} alt="activity" class="card--image"/>
+            <img src={props.item.coverImg} alt="activity" class="card--image"/>
             <div class="card--stats">
                 <img src={star} alt="star" class="card--star"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
